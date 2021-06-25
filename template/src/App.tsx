b922a360e6367bce496e2085daba92ba3f05144f
AJ <ashishshetty2612@gmail.com>
2021-06-25 15:32:29 +0530

    Feat: final setups

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import "./styles/App.css"
+import React from "react";
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Ashish shetty's custom template.
-    </div>
-  );
+  return <div className="App">Ashish shetty's custom template.</div>;
 }
 
 export default App;
